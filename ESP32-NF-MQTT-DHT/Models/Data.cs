12cbbd6366b31c1cc63c032116d8baf5423a2a60
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2022-11-08 17:51:06 +0200

    update data

--- ESP32-NF-MQTT-DHT/Models/Data.cs
+++ ESP32-NF-MQTT-DHT/Models/Data.cs
@@ -1,57 +1,23 @@
-﻿using System;
-
-namespace ESP32_NF_MQTT_DHT.Models
+﻿namespace ESP32_NF_MQTT_DHT.Models
 {
+    using System;
+
+    using Constants;
+
     public class Data
     {
-        public string ESP32 { get; set; }
-
-        public string Sensorname { get; set; }
+        public string Sensorname => "Esp32NF";
 
         public string Date { get; set; }
 
         public string Time { get; set; }
 
-        public int Sleep5Count { get; set; }
-
-        public int BootCount { get; set; }
-
-        public int Lux { get; set; }
-
         public double Temp { get; set; }
 
         public int Humid { get; set; }
 
-        public int Soil { get; set; }
+        public string Wifissid => Constants.SSID;
 
-        public int SoilTemp { get; set; }
-
-        public int Salt { get; set; }
-
-        public string Saltadvice { get; set; }
-
-        public int Bat { get; set; }
-
-        public string Batcharge { get; set; }
-
-        public string BatchargeDate { get; set; }
-
-        public double DaysOnBattery { get; set; }
-
-        public int Battvolt { get; set; }
-
-        public double Battvoltage { get; set; }
-
-        public int Pressure { get; set; }
-
-        public int PlantValveNo { get; set; }
-
-        public string? Wifissid { get; set; }
-
-        public string? Rel { get; set; }
-
-        public DateTime CurrentDate { get; set; }
-
-        public int Messages { get; set; }
+        public string Rel => "1.0 Init";
     }
 }
