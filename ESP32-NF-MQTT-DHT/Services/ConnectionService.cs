c312d1848880c62a67d3b7315fa1d36c72f30f73
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2022-11-08 23:07:32 +0200

    remove wifi scanning add detailed error msgs fix bug (ghost device)

--- ESP32-NF-MQTT-DHT/Services/ConnectionService.cs
+++ ESP32-NF-MQTT-DHT/Services/ConnectionService.cs
@@ -15,47 +15,60 @@
         {
             var wifiAdapter = WifiAdapter.FindAllAdapters()[0];
 
-            // Begin network scan.
-            wifiAdapter.ScanAsync();
-
-            // While networks are being scan, continue on configuration. If networks were set previously, 
-            // board may already be auto-connected, so reconnection is not even needed.
-            var ipAddress = NetworkInterface.GetAllNetworkInterfaces()[0].IPv4Address;
             var isConnected = false;
+            int count = 0;
+
             while (isConnected == false)
             {
-                foreach (var network in wifiAdapter.NetworkReport.AvailableNetworks)
+                Debug.WriteLine($"[*] Connecting... Attempt {++count}");
+
+                var result = wifiAdapter.Connect(
+                    Constants.SSID,
+                    WifiReconnectionKind.Automatic,
+                    Constants.WIFI_PASSWORD);
+
+                if (result.ConnectionStatus == WifiConnectionStatus.Success)
                 {
-                    // If its our Network then try to connect
-                    if (network.Ssid == Constants.SSID)
+                    Debug.WriteLine($"[+] Connected to Wifi network {Constants.SSID}.");
+                    Thread.Sleep(2000);
+                    break;
+                }
+                else
+                {
+                    // more detailed error message
+                    string errorMsg = string.Empty;
+
+                    switch (result.ConnectionStatus.ToString())
                     {
-                        var result = wifiAdapter.Connect(
-                            network,
-                            WifiReconnectionKind.Automatic,
-                            Constants.WIFI_PASSWORD);
-
-                        if (result.ConnectionStatus == WifiConnectionStatus.Success)
-                        {
-                            Debug.WriteLine($"[+] Connected to Wifi network {network.Ssid}.");
+                        case "0":
+                            errorMsg = "Access to the network has been revoked.";
+                            break;
+                        case "1":
+                            errorMsg = "Invalid credential was presented.";
+                            break;
+                        case "2":
+                            errorMsg = "Network is not available.";
+                            break;
+                        case "4":
+                            errorMsg = "Connection attempt timed out.";
+                            break;
+                        case "5":
+                            errorMsg = "Unspecified error";
                             isConnected = true;
-                        }
-                        else
-                        {
-                            Debug.WriteLine(
-                                $"[-] Error {result.ConnectionStatus} connecting to Wifi network {network.Ssid}.");
-                        }
+                            break;
+                        case "6":
+                            errorMsg = "Authentication protocol is not supported.";
+                            break;
                     }
-                }
 
-                if (isConnected == false)
-                {
-                    wifiAdapter.ScanAsync();
-                }
+                    Debug.WriteLine(
+                        $"[-] Connection failed [{errorMsg}]");
 
-                Thread.Sleep(10000);
+                    Thread.Sleep(10000);
+                }
             }
 
-            ipAddress = NetworkInterface.GetAllNetworkInterfaces()[0].IPv4Address;
+            var ipAddress = NetworkInterface.GetAllNetworkInterfaces()[0].IPv4Address;
             Debug.WriteLine($"[+] Connected to Wifi network {Constants.SSID} with IP address {ipAddress} ");
         }
     }
