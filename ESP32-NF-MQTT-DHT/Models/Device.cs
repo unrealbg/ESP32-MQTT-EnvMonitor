b60e30179bdbd12ecdfe04500425fe5dfbcf0415
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2022-11-08 17:39:51 +0200

    add device model

---
+++ ESP32-NF-MQTT-DHT/Models/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace ESP32_NF_MQTT_DHT.Models
+{
+    public class Device
+    {
+        public Device()
+        {
+            this.Data = new Data();
+        }
+
+        public Data Data { get; set; }
+    }
+}
