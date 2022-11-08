c7f8d533158524839d163cc728a964f2b5613cab
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2022-11-08 17:25:25 +0200

    add global constants

---
+++ ESP32-NF-MQTT-DHT/Constants/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace ESP32_NF_MQTT_DHT.Constants
+{
+    using System;
+
+    public static class Constants
+    {
+        public const string SSID = "MySSID";
+
+        public const string WIFI_PASSWORD = "password";
+
+        public static readonly string BROKER = "broker.mydomain.com";
+
+        public static readonly string CLIENT_ID = Guid.NewGuid().ToString();
+
+        public static readonly string MQTT_CLIENT_USERNAME = "username";
+
+        public static readonly string MQTT_CLIENT_PASSWORD = "password";
+    }
+}
