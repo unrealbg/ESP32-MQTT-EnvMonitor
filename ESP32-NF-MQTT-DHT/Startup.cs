56e78bf669e31ef90a3155a62189f11ebf32999c
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2022-11-08 17:10:46 +0200

    add startup/init services

---
+++ ESP32-NF-MQTT-DHT/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace ESP32_NF_MQTT_DHT
+{
+    using Microsoft.Extensions.Logging;
+
+    using Services.Contracts;
+
+    public class Startup
+    {
+        private readonly IConnectionService _connectionService;
+        private readonly IMqttClient _mqttClient;
+        private readonly IDhtService _dhtService;
+        private readonly ILogger _logger;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public Startup(
+            IConnectionService connectionService,
+            IMqttClient mqttClient,
+            IDhtService dhtService,
+            ILoggerFactory loggerFactory)
+        {
+            this._connectionService = connectionService;
+            this._mqttClient = mqttClient;
+            this._dhtService = dhtService;
+            this._logger = loggerFactory.CreateLogger(nameof(Startup));
+
+            this._logger.LogInformation("Initializing application...");
+        }
+
+        public void Run()
+        {
+            this._connectionService.Connect();
+            this._mqttClient.Start();
+            this._dhtService.Start();
+        }
+    }
+}
