dc38fb3bf320eec606eeb17a782d73b9b62f9c19
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2022-11-10 19:03:11 +0200

    rename

---
+++ ESP32-NF-MQTT-DHT/Models/Sensor.cs
@@ -0,0 +1,12 @@
+﻿namespace ESP32_NF_MQTT_DHT.Models
+{
+    public class Sensor
+    {
+        public Sensor()
+        {
+            this.Data = new Data();
+        }
+
+        public Data Data { get; set; }
+    }
+}
