038923518f296b3b63699f02fe0a09d4d93c28fb
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-16 15:52:13 +0200

    Refactored DhtService for Enhanced Readability and Error Management
    - Extracted sensor data reading and publishing logic into a separate method ReadAndPublishData.
    - Implemented UpdateDeviceData method for updating sensor data.
    - Added PublishError method for consistent error reporting to MQTT client.
    - Defined constants for time intervals and MQTT topics for improved maintainability.
    - Enhanced exception handling to include error publishing in case of sensor read failures.

--- ESP32-NF-MQTT-DHT/Services/DhtService.cs
+++ ESP32-NF-MQTT-DHT/Services/DhtService.cs
@@ -17,66 +17,74 @@
     internal class DhtService : IDhtService
     {
         private readonly IMqttClient _client;
+        private const int ReadInterval = 60000; // 1 minute
+        private const int ErrorInterval = 10000; // 10 seconds
+        private const string Topic = "IoT/messages2";
+        private const string ErrorTopic = "nf-mqtt/basic-dht";
 
         public DhtService(IMqttClient client)
         {
-            this._client = client;
-            this.JsonSerializer = new JsonSerializer();
-            this.Device = new Sensor();
+            _client = client;
+            JsonSerializer = new JsonSerializer();
+            Device = new Sensor();
         }
 
         public Sensor Device { get; set; }
 
         public JsonSerializer JsonSerializer { get; set; }
 
-        // Start Reading Sensor Data from DHT21 sensor
         public void Start()
         {
             Debug.WriteLine("[+] Start Reading Sensor Data from DHT21");
-            
+
             using (Dht21 dht = new Dht21(26, 27))
             {
                 while (true)
                 {
                     try
                     {
-                        var temperature = dht.Temperature.Value;
-                        var humidity = dht.Humidity.Value;
-
-                        // if the read is successful it sends data every minute
-                        if (dht.IsLastReadSuccessful)
-                        {
-                            this.Device.Data.Date = DateTime.UtcNow.Date.ToString("dd/MM/yyyy");
-                            this.Device.Data.Time = DateTime.UtcNow.ToString("HH:mm:ss");
-                            this.Device.Data.Temp = temperature;
-                            this.Device.Data.Humid = (int)humidity;
-
-                            var msg = JsonSerializer.SerializeObject(this.Device);
-
-                            this._client.MqttClient.Publish(
-                                "IoT/messages2",
-                                Encoding.UTF8.GetBytes(msg),
-                                MqttQoSLevel.AtLeastOnce,
-                                false);
-                            Thread.Sleep(60000);
-                        }
-                        else
-                        {
-                            Debug.WriteLine("Error reading DHT sensor");
-                            this._client.MqttClient.Publish(
-                                "nf-mqtt/basic-dht",
-                                Encoding.UTF8.GetBytes($"Error"),
-                                MqttQoSLevel.AtLeastOnce,
-                                false);
-                            Thread.Sleep(10000);
-                        }
+                        ReadAndPublishData(dht);
                     }
                     catch (Exception ex)
                     {
                         Debug.WriteLine(ex.Message);
+                        PublishError("Sensor reading error: " + ex.Message);
                     }
                 }
             }
+        }
+
+        private void ReadAndPublishData(Dht21 dht)
+        {
+            var temperature = dht.Temperature.Value;
+            var humidity = dht.Humidity.Value;
+
+            if (dht.IsLastReadSuccessful)
+            {
+                UpdateDeviceData(temperature, humidity);
+                var msg = JsonSerializer.SerializeObject(Device);
+                _client.MqttClient.Publish(Topic, Encoding.UTF8.GetBytes(msg), MqttQoSLevel.AtLeastOnce, false);
+                Thread.Sleep(ReadInterval);
+            }
+            else
+            {
+                Debug.WriteLine("Error reading DHT sensor");
+                PublishError("Error reading DHT sensor");
+            }
+        }
+
+        private void UpdateDeviceData(double temperature, double humidity)
+        {
+            Device.Data.Date = DateTime.UtcNow.Date.ToString("dd/MM/yyyy");
+            Device.Data.Time = DateTime.UtcNow.ToString("HH:mm:ss");
+            Device.Data.Temp = temperature;
+            Device.Data.Humid = (int)humidity;
+        }
+
+        private void PublishError(string errorMessage)
+        {
+            _client.MqttClient.Publish(ErrorTopic, Encoding.UTF8.GetBytes(errorMessage), MqttQoSLevel.AtLeastOnce, false);
+            Thread.Sleep(ErrorInterval);
         }
     }
 }
