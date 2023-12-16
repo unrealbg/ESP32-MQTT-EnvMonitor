1ef1f9684307298d51740ab47482b69052de9bd9
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-16 15:37:07 +0200

    Optimized and Improved Readability in Program.cs
    - Refactored ConfigureServices method to use generic service registration methods.
    - Added XML comments for classes and methods for enhanced documentation.
    - Improved readability by replacing 'typeof' with generic methods.
    - Minor adjustments for better code structure and style.

--- ESP32-NF-MQTT-DHT/Program.cs
+++ ESP32-NF-MQTT-DHT/Program.cs
@@ -8,24 +8,36 @@ namespace ESP32_NF_MQTT_DHT
     using Services;
     using Services.Contracts;
 
+    using System;
+
+    /// <summary>
+    /// Main program class.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main entry point for the application.
+        /// </summary>
         public static void Main()
         {
             var services = ConfigureServices();
-            var application = (Startup)services.GetRequiredService(typeof(Startup));
+            var application = services.GetRequiredService<Startup>();
             application.Run();
         }
 
+        /// <summary>
+        /// Configures services for the application.
+        /// </summary>
+        /// <returns>Configured service provider.</returns>
         private static ServiceProvider ConfigureServices()
         {
             return new ServiceCollection()
-                .AddSingleton(typeof(Startup))
-            .AddSingleton(typeof(IConnectionService), typeof(ConnectionService))
-                .AddSingleton(typeof(IMqttClient), typeof(MqttClientService))
-                .AddSingleton(typeof(IDhtService), typeof(DhtService))
-                .AddSingleton(typeof(IUptimeService), typeof(UptimeService))
-                .AddSingleton(typeof(ILoggerFactory), typeof(DebugLoggerFactory))
+                .AddSingleton<Startup>()
+                .AddSingleton<IConnectionService, ConnectionService>()
+                .AddSingleton<IMqttClient, MqttClientService>()
+                .AddSingleton<IDhtService, DhtService>()
+                .AddSingleton<IUptimeService, UptimeService>()
+                .AddSingleton<ILoggerFactory, DebugLoggerFactory>()
                 .BuildServiceProvider();
         }
     }
