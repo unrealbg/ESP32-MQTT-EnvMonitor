14196e144da33121b582d54e08ef2a6a7c249cbc
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-16 15:42:34 +0200

    Refactored Startup Class for Enhanced Error Handling and Logging
    - Implemented try-catch block in Run method for robust error handling.
    - Enhanced logging to provide detailed status updates during service startup.
    - Added null checks for dependencies in the constructor for improved reliability.
    - Improved code documentation with XML comments for better clarity and maintainability.

--- ESP32-NF-MQTT-DHT/Startup.cs
+++ ESP32-NF-MQTT-DHT/Startup.cs
@@ -4,33 +4,63 @@
 
     using Services.Contracts;
 
+    using System;
+
+    /// <summary>
+    /// Represents the startup process of the application.
+    /// </summary>
     public class Startup
     {
         private readonly IConnectionService _connectionService;
         private readonly IMqttClient _mqttClient;
         private readonly IDhtService _dhtService;
         private readonly ILogger _logger;
-        private readonly ILoggerFactory _loggerFactory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="connectionService">Service for managing connections.</param>
+        /// <param name="mqttClient">MQTT client service.</param>
+        /// <param name="dhtService">DHT sensor service.</param>
+        /// <param name="loggerFactory">Factory for creating logger instances.</param>
         public Startup(
             IConnectionService connectionService,
             IMqttClient mqttClient,
             IDhtService dhtService,
             ILoggerFactory loggerFactory)
         {
-            this._connectionService = connectionService;
-            this._mqttClient = mqttClient;
-            this._dhtService = dhtService;
-            this._logger = loggerFactory.CreateLogger(nameof(Startup));
+            _connectionService = connectionService ?? throw new ArgumentNullException(nameof(connectionService));
+            _mqttClient = mqttClient ?? throw new ArgumentNullException(nameof(mqttClient));
+            _dhtService = dhtService ?? throw new ArgumentNullException(nameof(dhtService));
+            _logger = loggerFactory?.CreateLogger(nameof(Startup)) ?? throw new ArgumentNullException(nameof(loggerFactory));
 
-            this._logger.LogInformation("Initializing application...");
+            _logger.LogInformation("Startup: Initializing application...");
         }
 
+        /// <summary>
+        /// Runs the application services.
+        /// </summary>
         public void Run()
         {
-            this._connectionService.Connect();
-            this._mqttClient.Start();
-            this._dhtService.Start();
+            try
+            {
+                _logger.LogInformation("Startup: Establishing connection...");
+                _connectionService.Connect();
+                _logger.LogInformation("Startup: Connection established.");
+
+                _logger.LogInformation("Startup: Starting MQTT client...");
+                _mqttClient.Start();
+                _logger.LogInformation("Startup: MQTT client started.");
+
+                _logger.LogInformation("Startup: Starting DHT service...");
+                _dhtService.Start();
+                _logger.LogInformation("Startup: DHT service started.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred during startup: {ex.Message}");
+                // Consider re-throwing the exception or handling it as needed for your application
+            }
         }
     }
 }
