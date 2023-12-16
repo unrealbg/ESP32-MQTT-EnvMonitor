c74738dda5bf0cb4c7788b79ca8911e089a01880
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-16 15:45:47 +0200

    Improved UptimeService for Continuous Time Tracking
    - Modified UptimeService to start the Stopwatch automatically in the constructor.
    - Replaced string concatenation with StringBuilder for efficient uptime string formatting.
    - Removed the Stopwatch.Stop() call from GetUptime method to ensure continuous time tracking.

--- ESP32-NF-MQTT-DHT/Services/UptimeService.cs
+++ ESP32-NF-MQTT-DHT/Services/UptimeService.cs
@@ -1,6 +1,7 @@
 ﻿namespace ESP32_NF_MQTT_DHT.Services
 {
     using System.Diagnostics;
+    using System.Text;
 
     using Services.Contracts;
 
@@ -9,17 +10,21 @@
         public UptimeService()
         {
             this.Stopwatch = new Stopwatch();
+            this.Stopwatch.Start();
         }
 
-        public Stopwatch Stopwatch { get; }
+        public Stopwatch Stopwatch { get; private set; }
 
         public string GetUptime()
         {
-            this.Stopwatch.Stop();
-            return this.Stopwatch.Elapsed.Days + " days, " +
-                   this.Stopwatch.Elapsed.Hours + " hours, " +
-                   this.Stopwatch.Elapsed.Minutes + " minutes, " +
-                   this.Stopwatch.Elapsed.Seconds + " seconds";
+            var elapsed = this.Stopwatch.Elapsed;
+            var uptimeStringBuilder = new StringBuilder();
+            uptimeStringBuilder.Append(elapsed.Days).Append(" days, ");
+            uptimeStringBuilder.Append(elapsed.Hours).Append(" hours, ");
+            uptimeStringBuilder.Append(elapsed.Minutes).Append(" minutes, ");
+            uptimeStringBuilder.Append(elapsed.Seconds).Append(" seconds");
+
+            return uptimeStringBuilder.ToString();
         }
     }
 }
