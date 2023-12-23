2e1a3daa7600600f3737c12ccbb0bf71a9104bd8
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-23 15:19:53 +0200

    feat: Add TcpListenerService for Remote TCP Connection Management
    This commit introduces the TcpListenerService class in the ESP32_NF_MQTT_DHT.Services namespace with the following features:
    - Establishment of a TCP listener to accept and manage incoming TCP connections on a specified port.
    - Authentication mechanism for clients based on predefined credentials.
    - Processing and execution of client commands including system uptime, temperature reading, MQTT publishing, and system reboot.
    - Integration with IUptimeService, IMqttClientService, and IDhtService to leverage existing sensor and MQTT functionalities.

---
+++ ESP32-NF-MQTT-DHT/Services/TcpListenerService.cs
@@ -0,0 +1,163 @@
+﻿using ESP32_NF_MQTT_DHT.Services.Contracts;
+
+using nanoFramework.Runtime.Native;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+using static ESP32_NF_MQTT_DHT.Constants.Constants;
+
+namespace ESP32_NF_MQTT_DHT.Services
+{
+    /// <summary>
+    /// Provides services for TCP listener functionalities including accepting and handling incoming TCP connections.
+    /// </summary>
+    public class TcpListenerService : ITcpListenerService
+    {
+        private const int TcpPort = 1234;
+        private const int Timeout = 1000;
+        private readonly IUptimeService _uptimeService;
+        private readonly IMqttClientService _mqttClient;
+        private readonly IDhtService _dhtService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TcpListenerService"/> class.
+        /// </summary>
+        /// <param name="uptimeService">Service to get uptime information.</param>
+        /// <param name="mqttClient">Service to handle MQTT client functionalities.</param>
+        /// <param name="dhtService">Service to interact with a DHT sensor.</param>
+        public TcpListenerService(IUptimeService uptimeService, IMqttClientService mqttClient, IDhtService dhtService)
+        {
+            _uptimeService = uptimeService;
+            _mqttClient = mqttClient;
+            _dhtService = dhtService;
+        }
+
+        /// <summary>
+        /// Starts the TCP listener service.
+        /// </summary>
+        /// <remarks>
+        /// This method initializes and starts a TCP listener that listens for incoming connections
+        /// on a specified port. It handles the connections in a separate thread.
+        /// </remarks>
+        public void Start()
+        {
+            Thread listenerThread = new Thread(StartTcpListening);
+            listenerThread.Start();
+        }
+
+        private void StartTcpListening()
+        {
+            var localIP = NetworkInterface.GetAllNetworkInterfaces()[0].IPv4Address;
+            TcpListener listener = new TcpListener(IPAddress.Any, TcpPort);
+            listener.Server.ReceiveTimeout = Timeout;
+            listener.Server.SendTimeout = Timeout;
+            listener.Start(2);
+
+            while (true)
+            {
+                Debug.WriteLine($"[+] Waiting for an incoming connection on {localIP} port {TcpPort}");
+
+                try
+                {
+                    using TcpClient client = listener.AcceptTcpClient();
+                    IPEndPoint remoteIpEndPoint = client.Client.RemoteEndPoint as IPEndPoint;
+                    string clientIp = remoteIpEndPoint!.Address.ToString();
+                    Debug.WriteLine($"[+] Client connected on port {TcpPort} from {clientIp}");
+
+                    using NetworkStream stream = client.GetStream();
+                    using StreamReader streamReader = new StreamReader(stream);
+                    using StreamWriter sw = new StreamWriter(stream);
+
+                    AuthenticateClient(streamReader, sw, clientIp);
+                    ProcessClientCommands(streamReader, sw, client, clientIp);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Exception:-{ex.Message}");
+                }
+            }
+        }
+
+        private void AuthenticateClient(StreamReader streamReader, StreamWriter sw, string clientIp)
+        {
+            sw.Write($"login as: ");
+            sw.Flush();
+            var usernameInput = streamReader.ReadLine();
+            sw.Write($"{usernameInput}@{clientIp} password: ");
+            sw.Flush();
+            var passwordInput = streamReader.ReadLine();
+
+            if (TCP_CLIENT_USERNAME != usernameInput || TCP_CLIENT_PASSWORD != passwordInput)
+            {
+                throw new ArgumentException("Invalid credentials.");
+            }
+        }
+
+        private void ProcessClientCommands(StreamReader streamReader, StreamWriter sw, TcpClient client, string clientIp)
+        {
+            WriteToStream(sw, $"Welcome to EspDuino-32 HW-729 (OpenSocket connection)\r\n\r\n *"
+                                + $" Documentation:  https://github.com/unrealbg/NF.Esp32.Mqtt.Dht21\r\n *"
+                                + $" Management:     https://github.com/unrealbg/NF.Esp32.Mqtt.Dht21\r\n *"
+                                + $" Support:        https://github.com/unrealbg/NF.Esp32.Mqtt.Dht21\r\n\r\n  "
+                                + $"System information as of {DateTime.Today} {DateTime.Today.Date} {DateTime.Today.Day} EET {DateTime.Today.Year}\r\n\r\n  "
+                                + $"Available commands: uptime, temp, publishUptime, exit, reboot\r\n\r\n  "
+                                + $"Users logged in:       1  IPv4 address for eth0: {NetworkInterface.GetAllNetworkInterfaces()[0].IPv4Address}\r\n");
+
+            sw.Write($"[{TCP_CLIENT_USERNAME}@{clientIp}]:~# ");
+            sw.Flush();
+
+            while (streamReader.Peek() > -1)
+            {
+                var command = streamReader.ReadLine();
+                Debug.WriteLine($"[u] {command}");
+
+                if (ProcessCommand(command, sw, client))
+                {
+                    break;
+                }
+
+                sw.Write($"[{TCP_CLIENT_USERNAME}@{clientIp}] ");
+                sw.Flush();
+            }
+        }
+
+        private bool ProcessCommand(string command, StreamWriter sw, TcpClient client)
+        {
+            switch (command)
+            {
+                case "uptime":
+                    WriteToStream(sw, _uptimeService.GetUptime());
+                    return false;
+                case "temp":
+                    WriteToStream(sw, _dhtService.GetTemp());
+                    return false;
+                case "publishUptime":
+                    _mqttClient.MqttClient.Publish($"home/{DEVICE}/uptime", Encoding.UTF8.GetBytes(_uptimeService.GetUptime()));
+                    return false;
+                case "exit":
+                    Debug.WriteLine("[-] Client disconnected!");
+                    client.Close();
+                    return true;
+                case "reboot":
+                    Power.RebootDevice();
+                    return true;
+                default:
+                    WriteToStream(sw, "[-] Unrecognized command");
+                    return false;
+            }
+        }
+
+        private void WriteToStream(StreamWriter writer, string text)
+        {
+            writer.WriteLine(text);
+            writer.Flush();
+        }
+    }
+}
