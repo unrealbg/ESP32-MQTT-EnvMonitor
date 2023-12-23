24e2d9f1516fe4eeeb4c5240863be850bb5877f8
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-23 19:07:07 +0200

    feat: Implement WebServerService with DHT sensor integration
    Created IWebServerService interface and WebServerService class for handling web server operations. Integrated IDhtService within WebServerService to provide temperature and humidity data through HTTP endpoints '/api/temperature' and '/api/humidity'. This implementation enhances the project's capability to serve sensor data over a web interface.

---
+++ ESP32-NF-MQTT-DHT/Services/WebServerService.cs
@@ -0,0 +1,39 @@
+﻿using ESP32_NF_MQTT_DHT.Services.Contracts;
+
+using nanoFramework.WebServer;
+
+namespace ESP32_NF_MQTT_DHT.Services
+{
+    public class WebServerService : IWebServerService
+    {
+        private WebServer server;
+        private readonly IDhtService _dhtService;
+
+        public WebServerService(int port, IDhtService dhtService)
+        {
+            _dhtService = dhtService;
+            server = new WebServer(port, HttpProtocol.Http);
+            server.CommandReceived += ServerCommandReceived;
+        }
+
+        private void ServerCommandReceived(object sender, WebServerEventArgs e)
+        {
+            switch (e.Context.Request.RawUrl)
+            {
+                case "/api/temperature":
+                    var temperature = _dhtService.GetTemp();
+                    WebServer.OutPutStream(e.Context.Response, temperature);
+                    break;
+                case "/api/humidity":
+                    var humidity = _dhtService.GetHumidity();
+                    WebServer.OutPutStream(e.Context.Response, humidity);
+                    break;
+            }
+        }
+
+        public void Start()
+        {
+            server.Start();
+        }
+    }
+}
