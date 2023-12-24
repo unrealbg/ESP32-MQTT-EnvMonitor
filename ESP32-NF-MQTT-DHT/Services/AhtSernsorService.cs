b93519969441768a3ac8220d00a888ed05e204d1
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-24 22:25:18 +0200

    feat: Add AhtSensorService for AHT10 Sensor Integration
    - Implemented AhtSensorService to integrate AHT10 temperature and humidity sensor.
    - Configured I2C communication with ESP32.
    - Enhanced data acquisition with continuous sensor reading in a dedicated thread.
    - Enabled MQTT publishing of sensor data for remote monitoring.
    - Incorporated error handling for abnormal sensor readings and MQTT error publishing.
    - Provided methods for accessing real-time temperature and humidity data.
    - Improved system robustness and data reliability with AHT10 sensor integration.

---
+++ ESP32-NF-MQTT-DHT/Services/AhtSernsorService.cs
@@ -0,0 +1,88 @@
+﻿namespace ESP32_NF_MQTT_DHT.Services
+{
+    using Contracts;
+    using Models;
+
+    using nanoFramework.Hardware.Esp32;
+    using nanoFramework.Logging;
+    using nanoFramework.Json;
+
+    using Iot.Device.Ahtxx;
+
+    using System;
+    using System.Device.I2c;
+    using System.Text;
+    using System.Threading;
+
+    using Microsoft.Extensions.Logging;
+
+    using static Constants.Constants;
+
+    public class AhtSensorService : IAhtSernsorService
+    {
+        private readonly IMqttClientService _mqttClientService;
+        private double _temperature;
+        private double _humidity;
+
+        public AhtSensorService(IMqttClientService mqttClientService)
+        {
+            _mqttClientService = mqttClientService;
+        }
+
+        public Sensor Sensor { get; set; }
+
+        public void Start()
+        {
+            Configuration.SetPinFunction(4, DeviceFunction.I2C1_DATA);
+            Configuration.SetPinFunction(5, DeviceFunction.I2C1_CLOCK);
+
+            Thread sensorReadThread = new Thread(StartReceivingData);
+            sensorReadThread.Start();
+        }
+
+        public double GetTemp() => _temperature;
+        public double GetHumidity() => _humidity;
+
+        private void StartReceivingData()
+        {
+            I2cConnectionSettings i2CSettings = new I2cConnectionSettings(1, AhtBase.DefaultI2cAddress);
+            I2cDevice i2CDevice = I2cDevice.Create(i2CSettings);
+
+            using (var aht = new Aht10(i2CDevice))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        _temperature = aht.GetTemperature().DegreesCelsius;
+                        _humidity = aht.GetHumidity().Percent;
+
+                        if (_temperature < 45)
+                        {
+                            var deviceData = new
+                            {
+                                Date = DateTime.UtcNow.ToString("dd/MM/yyyy"),
+                                Time = DateTime.UtcNow.ToString("HH:mm:ss"),
+                                Temp = _temperature,
+                                Humid = (int)_humidity
+                            };
+
+                            var msg = JsonSerializer.SerializeObject(deviceData);
+                            _mqttClientService.MqttClient.Publish("IoT/messages", Encoding.UTF8.GetBytes(msg));
+                        }
+                        else
+                        {
+                            _mqttClientService.MqttClient.Publish($"home/{Device}/errors", Encoding.UTF8.GetBytes("Error reading from DHT sensor"));
+                        }
+
+                        Thread.Sleep(60000); // Adjust time as needed
+                    }
+                    catch (Exception ex)
+                    {
+                        aht.GetCurrentClassLogger().LogError($"[e] ERROR: {ex.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ESP32-NF-MQTT-DHT/Services/Contracts/IAhtSernsorService.cs
@@ -0,0 +1,11 @@
+﻿namespace ESP32_NF_MQTT_DHT.Services.Contracts
+{
+    public interface IAhtSernsorService
+    {
+        void Start();
+
+        double GetTemp();
+
+        double GetHumidity();
+    }
+}
