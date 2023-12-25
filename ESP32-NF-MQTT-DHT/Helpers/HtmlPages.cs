c744ead74f2eb8f8f6f0e22b87082155aa797ee7
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-25 20:15:36 +0200

    feat: Add HtmlPages Helper with Interactive Index Page for Web Server
    - Introduced `HtmlPages` class as a utility to manage HTML content within the project.
    - Implemented `IndexPage` as a static string containing HTML structure for the ESP32 sensor dashboard.
    - The HTML content features a clean and responsive design with CSS for styling.
    - Incorporated JavaScript to fetch and display temperature and humidity data dynamically on the web interface.
    - Implemented AJAX calls to periodically fetch sensor data from `/api/temperature` and `/api/humidity` endpoints.
    - Set a refresh interval of 5 seconds for live updates of sensor data on the web page.
    - This addition aims to enhance the user experience by providing a real-time, visual representation of sensor readings.

---
+++ ESP32-NF-MQTT-DHT/Helpers/HtmlPages.cs
@@ -0,0 +1,57 @@
+﻿namespace ESP32_NF_MQTT_DHT.Helpers
+{
+    public static class HtmlPages
+    {
+        public static string IndexPage = @"
+            <html>
+            <head>
+                <title>ESP32 Sensor Dashboard</title>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f4f4f4;
+                        text-align: center;
+                        padding: 50px;
+                    }
+                    h1 {
+                        color: #333;
+                    }
+                    .sensor-data {
+                        margin-top: 20px;
+                        padding: 10px;
+                        background-color: #e0e0e0;
+                        display: inline-block;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>ESP32 Sensor Dashboard</h1>
+                <div class='sensor-data'>
+                    <h2>Temperature</h2>
+                    <p id='temperature'>Loading...</p>
+                </div>
+                <div class='sensor-data'>
+                    <h2>Humidity</h2>
+                    <p id='humidity'>Loading...</p>
+                </div>
+                <script>
+                    function fetchData(url, elementId) {
+                        var xhr = new XMLHttpRequest();
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState === 4 && xhr.status === 200) {
+                                var data = JSON.parse(xhr.responseText);
+                                document.getElementById(elementId).innerText = data[elementId] + (elementId === 'temperature' ? '°C' : '%');
+                            }
+                        };
+                        xhr.open('GET', url, true);
+                        xhr.send();
+                    }
+                    setInterval(function() {
+                        fetchData('/api/temperature', 'temperature');
+                        fetchData('/api/humidity', 'humidity');
+                    }, 5000); // Fetch data every 5 seconds
+                </script>
+            </body>
+            </html>";
+    }
+}
