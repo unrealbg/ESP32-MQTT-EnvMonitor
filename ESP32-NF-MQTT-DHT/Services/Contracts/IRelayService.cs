ef30bbce1f86c197e2ed27669e47dcff9b531500
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-26 01:03:47 +0200

    Add XML Documentation to Services in ESP32 MQTT Project
    - Added XML documentation for IRelayService and IWebServerService interfaces.
    - Added XML documentation for RelayService and WebServerService classes.

--- ESP32-NF-MQTT-DHT/Services/Contracts/IRelayService.cs
+++ ESP32-NF-MQTT-DHT/Services/Contracts/IRelayService.cs
@@ -1,9 +1,18 @@
 ﻿namespace ESP32_NF_MQTT_DHT.Services.Contracts
 {
+    /// <summary>
+    /// Interface for relay control services.
+    /// </summary>
     public interface IRelayService
     {
+        /// <summary>
+        /// Turns the relay on.
+        /// </summary>
         void TurnOn();
 
+        /// <summary>
+        /// Turns the relay off.
+        /// </summary>
         void TurnOff();
     }
 }
