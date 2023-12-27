ad19ef7d0df859e3a09691dae36d3f7a9f261a12
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2023-12-27 12:21:41 +0200

    Implement AhtSensorService for Enhanced Sensor Readings
    - Introduced IAhtSensorService interface with Start, GetTemp, and GetHumidity methods.
    - Developed AhtSensorService class implementing IAhtSensorService, leveraging Aht10 sensor for temperature and humidity readings.
    - Integrated MQTT client service for publishing sensor data and error messages.
    - Configured I2C settings and initiated a continuous sensor data reading thread in the Start method.
    - Implemented logic to publish sensor data to MQTT topic "IoT/messages" and error messages to "home/{Device}/errors".
    - Added detailed error logging within the sensor data reading loop.
    - Set a regular interval for sensor readings and message publishing.
    - Enhanced sensor data management with accurate and efficient readings from the Aht10 sensor.

---
+++ ESP32-NF-MQTT-DHT/Services/Contracts/IAhtSensorService.cs
@@ -0,0 +1,11 @@
+﻿namespace ESP32_NF_MQTT_DHT.Services.Contracts
+{
+    public interface IAhtSensorService
+    {
+        void Start();
+
+        double GetTemp();
+
+        double GetHumidity();
+    }
+}
