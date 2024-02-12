d9d846b6ef8555ddb80308eafb5c0a408cfa5946
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-02-12 20:02:04 +0200

    Enhance SensorController with API documentation route and improved error handling
    - Added `/documentation` route with `GetDocPage` method to serve API documentation, enhancing the usability and accessibility of the API for developers.
    - Improved error handling in `GetTemperature` and `GetHumidity` methods to provide more descriptive error messages, improving the API's robustness.
    - Implemented temperature range validation in `GetTemperature` endpoint to ensure data integrity by rejecting out-of-range values.
    - Centralized JSON response construction in `SendResponse` method to reduce code duplication and simplify JSON responses across API endpoints.
    - Introduced `SendPage` utility method for serving HTML content, streamlining the process of responding with HTML pages.
    - Specified appropriate content types for API responses (`application/json` for JSON and `text/html` for HTML content), aligning with web standards and enhancing client compatibility.

--- ESP32-NF-MQTT-DHT/Controllers/SensorController.cs
+++ ESP32-NF-MQTT-DHT/Controllers/SensorController.cs
@@ -10,6 +10,7 @@
     using nanoFramework.WebServer;
 
     using System;
+    using System.Net;
 
     public class SensorController
     {
@@ -24,46 +25,96 @@
         [Method("GET")]
         public void GetIndexPage(WebServerEventArgs e)
         {
-            var htmlContent = HtmlPages.IndexPage;
+            SendPage(e, HtmlPages.IndexPage);
+        }
 
-            e.Context.Response.ContentType = "text/html";
-            WebServer.OutPutStream(e.Context.Response, htmlContent);
+        [Route("/documentation")]
+        [Method("GET")]
+        public void GetDocPage(WebServerEventArgs e)
+        {
+            SendPage(e, HtmlPages.DocPage);
         }
 
         [Route("api/temperature")]
         [Method("GET")]
         public void GetTemperature(WebServerEventArgs e)
         {
-            var temperature = FetchTemperature();
-            RespondWithJson(e, $"{{\"temperature\": {temperature:f2}}}");
+            try
+            {
+                var temperature = FetchTemperature();
+                if (IsValidTemperature(temperature))
+                {
+                    var jsonResponse = $"{{\"temperature\": {temperature:f2}}}";
+                    SendResponse(e, jsonResponse, "application/json");
+                }
+                else
+                {
+                    SendErrorResponse(e, "Temperature data is out of expected range.", HttpStatusCode.InternalServerError);
+                }
+            }
+            catch (Exception ex)
+            {
+                SendErrorResponse(e, "An unexpected error occurred.", HttpStatusCode.InternalServerError);
+                _logger.LogError($"An unexpected error occurred: {ex.Message}");
+            }
         }
 
         [Route("api/humidity")]
         [Method("GET")]
         public void GetHumidity(WebServerEventArgs e)
         {
-            var humidity = FetchHumidity();
-            RespondWithJson(e, $"{{\"humidity\": {humidity:f1}}}");
+            try
+            {
+                var humidity = FetchHumidity();
+                if (!double.IsNaN(humidity))
+                {
+                    var jsonResponse = $"{{\"humidity\": {humidity:f1}}}";
+                    SendResponse(e, jsonResponse, "application/json");
+                }
+                else
+                {
+                    SendErrorResponse(e, "Humidity data is unavailable.", HttpStatusCode.InternalServerError);
+                }
+            }
+            catch (Exception ex)
+            {
+                SendErrorResponse(e, $"An unexpected error occurred: {ex.Message}", HttpStatusCode.InternalServerError);
+            }
         }
 
         [Route("api/data")]
         [Method("GET")]
         public void GetData(WebServerEventArgs e)
         {
-            var temperature = FetchTemperature();
-            var humidity = FetchHumidity();
-            var sensorData = new Sensor
+            try
             {
-                Data = new Data
+                var temperature = FetchTemperature();
+                var humidity = FetchHumidity();
+                if (!double.IsNaN(temperature) && !double.IsNaN(humidity))
                 {
-                    Date = DateTime.UtcNow.Date.ToString("dd/MM/yyyy"),
-                    Time = DateTime.UtcNow.ToString("HH:mm:ss"),
-                    Temp = temperature,
-                    Humid = (int)humidity
-                }
-            };
+                    var sensorData = new Sensor
+                    {
+                        Data = new Data
+                        {
+                            Date = DateTime.UtcNow.ToString("dd/MM/yyyy"),
+                            Time = DateTime.UtcNow.ToString("HH:mm:ss"),
+                            Temp = temperature,
+                            Humid = (int)humidity
+                        }
+                    };
 
-            RespondWithJson(e, JsonSerializer.SerializeObject(sensorData));
+                    var jsonResponse = JsonSerializer.SerializeObject(sensorData);
+                    SendResponse(e, jsonResponse, "application/json");
+                }
+                else
+                {
+                    SendErrorResponse(e, "Sensor data is unavailable.", HttpStatusCode.InternalServerError);
+                }
+            }
+            catch (Exception ex)
+            {
+                SendErrorResponse(e, $"An unexpected error occurred: {ex.Message}", HttpStatusCode.InternalServerError);
+            }
         }
 
         private double FetchTemperature()
@@ -94,10 +145,35 @@
             }
         }
 
-        private void RespondWithJson(WebServerEventArgs e, string jsonResponse)
+        private void SendResponse(WebServerEventArgs e, string content, string contentType)
         {
-            e.Context.Response.ContentType = "application/json";
-            WebServer.OutPutStream(e.Context.Response, jsonResponse);
+            try
+            {
+                e.Context.Response.ContentType = contentType;
+                WebServer.OutPutStream(e.Context.Response, content);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to send response: {ex.Message}");
+            }
+        }
+
+        private void SendPage(WebServerEventArgs e, string page)
+        {
+            SendResponse(e, page, "text/html");
+        }
+
+        private void SendErrorResponse(WebServerEventArgs e, string logMessage, HttpStatusCode statusCode)
+        {
+            var clientMessage = "An error occurred. Please try again later.";
+            e.Context.Response.StatusCode = (int)statusCode;
+            SendResponse(e, $"{{\"error\": \"{clientMessage}\"}}", "application/json");
+            _logger.LogError(logMessage);
+        }
+
+        private bool IsValidTemperature(double temperature)
+        {
+            return temperature >= -40 && temperature <= 85;
         }
     }
 }
