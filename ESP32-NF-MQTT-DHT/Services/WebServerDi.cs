c02b2660d0c85d26dac5bfb198b7c6138cbab5d9
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-02-20 20:00:51 +0200

    feat: Add WebServerDi class for DI support in web server

---
+++ ESP32-NF-MQTT-DHT/Services/WebServerDi.cs
@@ -0,0 +1,24 @@
+﻿namespace ESP32_NF_MQTT_DHT.Services
+{
+    using System;
+    using System.Net;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using nanoFramework.WebServer;
+
+    internal class WebServerDi : WebServer
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public WebServerDi(int port, HttpProtocol protocol, Type[] controllers, IServiceProvider serviceProvider) : base(port, protocol, controllers)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override void InvokeRoute(CallbackRoutes route, HttpListenerContext context)
+        {
+            route.Callback.Invoke(ActivatorUtilities.CreateInstance(_serviceProvider, route.Callback.DeclaringType), new object[] { new WebServerEventArgs(context) });
+        }
+    }
+}
