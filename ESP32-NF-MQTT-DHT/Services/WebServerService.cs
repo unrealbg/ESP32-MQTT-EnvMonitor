9ce5098ee5e3bdf217cc5b42dc23358c9c1c1d50
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-02-20 20:02:18 +0200

    refactor(webserver): Integrate DI and improve server management
    - Integrate dependency injection support into WebServerService by incorporating IServiceProvider for creating controller instances dynamically. This change enables more flexible and maintainable code by allowing services to be injected into controllers.
    - Enhance server lifecycle management by explicitly disposing of the WebServer instance before reinitialization to prevent resource leaks and ensure clean restarts. This includes modifications to the Start, Stop, and Restart methods to properly manage the WebServer instance.
    - Update ConnectionLost and ConnectionRestored event handlers to use the improved lifecycle management methods, ensuring the web server responds appropriately to connection changes.

--- ESP32-NF-MQTT-DHT/Services/WebServerService.cs
+++ ESP32-NF-MQTT-DHT/Services/WebServerService.cs
@@ -2,79 +2,101 @@
 {
     using System;
 
-    using Controllers;
     using Contracts;
+
+    using Controllers;
 
     using Microsoft.Extensions.Logging;
 
     using nanoFramework.WebServer;
 
+    /// <summary>
+    /// Provides services for managing the web server.
+    /// </summary>
     public class WebServerService : IWebServerService
     {
+        private readonly IServiceProvider _serviceProvider;
         private readonly IConnectionService _connectionService;
         private readonly ILogger _logger;
         private WebServer _server;
         private bool _isServerRunning = false;
 
-        public WebServerService(IConnectionService connectionService, ILoggerFactory loggerFactory)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebServerService"/> class.
+        /// </summary>
+        /// <param name="connectionService">The connection service.</param>
+        /// <param name="loggerFactory">The logger factory.</param>
+        /// <param name="serviceProvider">The service provider.</param>
+        public WebServerService(IConnectionService connectionService, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
         {
             _logger = loggerFactory?.CreateLogger(nameof(WebServerService)) ?? throw new ArgumentNullException(nameof(loggerFactory));
             _connectionService = connectionService ?? throw new ArgumentNullException(nameof(connectionService));
+            _serviceProvider = serviceProvider;
 
-            InitializeWebServer();
-
-            _connectionService.ConnectionRestored += ConnectionRestored;
-            _connectionService.ConnectionLost += ConnectionLost;
+            _connectionService.ConnectionRestored += this.ConnectionRestored;
+            _connectionService.ConnectionLost += this.ConnectionLost;
         }
 
+        /// <summary>
+        /// Starts the web server.
+        /// </summary>
         public void Start()
         {
             if (!_isServerRunning)
             {
+                this.InitializeWebServer();
                 _server.Start();
                 _isServerRunning = true;
                 _logger.LogInformation("Web server started.");
             }
         }
 
+        /// <summary>
+        /// Stops the web server.
+        /// </summary>
         public void Stop()
         {
             if (_isServerRunning)
             {
                 _server.Stop();
+                _server.Dispose();
+                _server = null;
                 _isServerRunning = false;
                 _logger.LogInformation("Web server stopped.");
             }
         }
 
+        /// <summary>
+        /// Restarts the web server.
+        /// </summary>
         public void Restart()
         {
-            Stop();
-            InitializeWebServer();
-            Start();
+            this.Stop();
+            this.Start();
         }
 
         private void ConnectionLost(object sender, EventArgs e)
         {
             _logger.LogInformation("Connection lost. Stopping the web server.");
-            Stop();
+            this.Stop();
         }
 
         private void ConnectionRestored(object sender, EventArgs e)
         {
             _logger.LogInformation("Connection restored. Starting the web server.");
-            Restart();
+            this.Restart();
         }
 
+        /// <summary>
+        /// Initializes the web server.
+        /// </summary>
         private void InitializeWebServer()
         {
-            if (_server != null)
+            if (_server == null)
             {
-                _server.Dispose();
-                _server = null;
+                _server = new WebServerDi(80, HttpProtocol.Http, new Type[] { typeof(SensorController) }, _serviceProvider);
+                _logger.LogInformation("Web server initialized.");
             }
-
-            _server = new WebServer(80, HttpProtocol.Http, new Type[] { typeof(SensorController) });
         }
     }
 }
