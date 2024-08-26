e865eb23d39a6ad7582f7743492375b911aa891d
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-08-26 22:47:43 +0300

    Feature: Add LogHelper Utility for Enhanced Logging with Timestamps
    This commit introduces a new `LogHelper` class in the `Helpers` namespace to improve logging functionality by including timestamps in log messages.
    
    Key changes include:
    - Created `LogHelper` class to encapsulate logging with timestamp functionality.
    - Added `LogWithTimestamp` method, which logs messages with the current timestamp, improving traceability and debugging.
    - Utilized `ILoggerFactory` for dynamic logger creation based on the provided logger name.
    - Included `TimeHelper` to retrieve the current timestamp for log entries.
    
    This enhancement provides a standardized way to include timestamps in log messages, improving the clarity and usefulness of logs during debugging and monitoring.

---
+++ ESP32-NF-MQTT-DHT/Helpers/LogHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace ESP32_NF_MQTT_DHT.Helpers
+{
+    using Microsoft.Extensions.Logging;
+
+    using static TimeHelper;
+
+    public class LogHelper
+    {
+        private readonly ILogger _logger;
+
+        public LogHelper(ILoggerFactory loggerFactory, string loggerName)
+        {
+            _logger = loggerFactory.CreateLogger(loggerName);
+        }
+
+        public void LogWithTimestamp(LogLevel level, string message)
+        {
+            _logger.Log(level, $"[{GetCurrentTimestamp()}] {message}");
+        }
+    }
+}
