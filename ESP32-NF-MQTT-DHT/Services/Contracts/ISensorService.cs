f310e3b56acf9510a549538c8027c25d2734ff85
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-09-06 16:27:10 +0300

    Add ISensorService interface with sensor control methods
    A new interface `ISensorService` has been added to the
    `ESP32_NF_MQTT_DHT.Services.Contracts` namespace. The interface
    includes methods to start and stop the sensor service, retrieve
    an array of sensor data, and get specific temperature and
    humidity data.

---
+++ ESP32-NF-MQTT-DHT/Services/Contracts/ISensorService.cs
@@ -0,0 +1,15 @@
+﻿namespace ESP32_NF_MQTT_DHT.Services.Contracts
+{
+    public interface ISensorService
+    {
+        void Start();
+
+        void Stop();
+
+        double[] GetData();
+
+        double GetTemp();
+
+        double GetHumidity();
+    }
+}
