d8ab3d4874cbc357a5b783b4a3a5c6c649cedf1e
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-09-06 16:31:22 +0300

    Update GetCurrentTimestamp method in TimeHelper class
    Modified GetCurrentTimestamp to add 3 hours to UTC time and
    changed the format to "dd-MM-yyyy HH:mm:ss".

--- ESP32-NF-MQTT-DHT/Helpers/TimeHelper.cs
+++ ESP32-NF-MQTT-DHT/Helpers/TimeHelper.cs
@@ -6,7 +6,7 @@
     {
         public static string GetCurrentTimestamp()
         {
-            return DateTime.UtcNow.AddHours(2).ToString("HH:mm:ss");
+            return DateTime.UtcNow.AddHours(3).ToString("dd-MM-yyy HH:mm:ss");
         }
     }
 }
