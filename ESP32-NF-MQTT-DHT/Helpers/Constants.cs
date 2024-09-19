9c177d821a022fd1769d2a50f953b475a201dc5b
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-09-19 23:06:10 +0300

    Update constants: modify ErrorInterval, add ReadInterval
    Modified the ErrorInterval constant from 10000 to 30000.
    Added a new constant ReadInterval with a value of 60000.

--- ESP32-NF-MQTT-DHT/Helpers/Constants.cs
+++ ESP32-NF-MQTT-DHT/Helpers/Constants.cs
@@ -4,9 +4,11 @@
     {
         public const int MaxReconnectAttempts = 20;
         public const int ReconnectDelay = 10000;
-        public const int ErrorInterval = 10000;
         public const int SensorDataInterval = 300000;
         public const double InvalidTemperature = -50;
         public const double InvalidHumidity = -100;
+
+        public const int ReadInterval = 60000;
+        public const int ErrorInterval = 30000;
     }
 }
