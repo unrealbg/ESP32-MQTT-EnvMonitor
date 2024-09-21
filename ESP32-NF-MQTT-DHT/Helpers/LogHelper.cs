667fa90fddc901004fb8234fdf7c113966473887
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2024-09-21 13:19:36 +0300

    Add XML docs to LogHelper, TimeHelper; fix date format
    Added XML documentation comments to the LogHelper and TimeHelper classes and their methods to provide descriptions and parameter details. Corrected the date format in the GetCurrentTimestamp method from "dd-MM-yyy HH:mm:ss" to "dd-MM-yyyy HH:mm:ss" in the TimeHelper class.

--- ESP32-NF-MQTT-DHT/Helpers/LogHelper.cs
+++ ESP32-NF-MQTT-DHT/Helpers/LogHelper.cs
@@ -5,13 +5,25 @@
 
     using static TimeHelper;
 
+    /// <summary>
+    /// Helper class for logging messages with timestamps.
+    /// </summary>
     public class LogHelper
     {
+        /// <summary>
+        /// Logs a message with the current timestamp.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         public void LogWithTimestamp(string message)
         {
             Debug.WriteLine($"[{GetCurrentTimestamp()}] {message}");
         }
 
+        /// <summary>
+        /// Logs an exception and a message with the current timestamp.
+        /// </summary>
+        /// <param name="ex">The exception to log.</param>
+        /// <param name="message">The message to log.</param>
         public void LogWithTimestamp(Exception ex, string message)
         {
             Debug.WriteLine($"{GetCurrentTimestamp()} - {ex} - {message}");
--- ESP32-NF-MQTT-DHT/Helpers/TimeHelper.cs
+++ ESP32-NF-MQTT-DHT/Helpers/TimeHelper.cs
@@ -2,11 +2,18 @@
 {
     using System;
 
+    /// <summary>
+    /// Helper class for time-related functionalities.
+    /// </summary>
     public static class TimeHelper
     {
+        /// <summary>
+        /// Gets the current timestamp in "dd-MM-yyyy HH:mm:ss" format.
+        /// </summary>
+        /// <returns>A string representing the current timestamp.</returns>
         public static string GetCurrentTimestamp()
         {
-            return DateTime.UtcNow.ToString("dd-MM-yyy HH:mm:ss");
+            return DateTime.UtcNow.ToString("dd-MM-yyyy HH:mm:ss");
         }
     }
 }
