82fefb62ad5015c78c1e4bcfba81343057e66e50
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-01-06 13:12:56 +0200

    Add SensorDataPublisher class for timed data publishing
    Introduces the `SensorDataPublisher` class in the
    `ESP32_NF_MQTT_DHT.Services.MQTT` namespace. This class
    handles the publishing of sensor data at regular intervals
    using a timer. It includes methods to start and stop the
    timer, and utilizes a `LogHelper` for logging events
    related to the timer's operation.

---
+++ ESP32-NF-MQTT-DHT/Services/MQTT/SensorDataPublisher.cs
@@ -0,0 +1,40 @@
+﻿namespace ESP32_NF_MQTT_DHT.Services.MQTT
+{
+    using System.Threading;
+
+    using ESP32_NF_MQTT_DHT.Helpers;
+
+    /// <summary>
+    /// Publishes sensor data at regular intervals.
+    /// </summary>
+    internal class SensorDataPublisher
+    {
+        private readonly LogHelper _logHelper = new LogHelper();
+        private readonly TimerCallback _publishCallback;
+        private Timer _timer;
+
+        public SensorDataPublisher(TimerCallback publishCallback)
+        {
+            this._publishCallback = publishCallback;
+        }
+
+        public void Start(int intervalMs)
+        {
+            if (this._timer == null)
+            {
+                this._timer = new Timer(this._publishCallback, null, 0, intervalMs);
+                this._logHelper.LogWithTimestamp("Sensor data timer started successfully.");
+            }
+        }
+
+        public void Stop()
+        {
+            if (this._timer != null)
+            {
+                this._timer.Dispose();
+                this._timer = null;
+                this._logHelper.LogWithTimestamp("Sensor data timer stopped.");
+            }
+        }
+    }
+}
