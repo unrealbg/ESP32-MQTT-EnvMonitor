5e69623cc1f7fdbe4f77067d74472a4fcbd5cd80
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-01-07 15:22:06 +0200

    Add ServiceCollectionExtensions for service registration
    Introduce a new static class `ServiceCollectionExtensions` in the `ESP32_NF_MQTT_DHT.Extensions` namespace. This class provides extension methods for the `IServiceCollection` interface, enabling the registration of services related to sensor management and MQTT functionality.
    
    Two methods are added: `AddSensorServices` for registering a sensor service based on `SensorType`, and `AddCoreServices` for registering core services such as connection, MQTT client, relay, uptime, web server, and internet connection services, all as singletons.

---
+++ ESP32-NF-MQTT-DHT/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace ESP32_NF_MQTT_DHT.Extensions
+{
+    using ESP32_NF_MQTT_DHT.Helpers;
+    using ESP32_NF_MQTT_DHT.Managers;
+    using ESP32_NF_MQTT_DHT.Managers.Contracts;
+    using ESP32_NF_MQTT_DHT.Services;
+    using ESP32_NF_MQTT_DHT.Services.Contracts;
+    using ESP32_NF_MQTT_DHT.Services.MQTT;
+    using ESP32_NF_MQTT_DHT.Services.MQTT.Contracts;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Extension methods for the <see cref="IServiceCollection"/> interface.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSensorServices(this IServiceCollection services, SensorType sensorType)
+        {
+            services.AddSingleton(typeof(ISensorService), SensorServiceFactory.GetSensorServiceType(sensorType));
+            return services;
+        }
+
+        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        {
+            services.AddSingleton(typeof(IConnectionService), typeof(ConnectionService));
+            services.AddSingleton(typeof(IMqttClientService), typeof(MqttClientService));
+            services.AddSingleton(typeof(IRelayService), typeof(RelayService));
+            services.AddSingleton(typeof(IUptimeService), typeof(UptimeService));
+            services.AddSingleton(typeof(IWebServerService), typeof(WebServerService));
+            services.AddSingleton(typeof(IInternetConnectionService), typeof(InternetConnectionService));
+            services.AddSingleton(typeof(MqttMessageHandler));
+            services.AddSingleton(typeof(ISensorManager), typeof(SensorManager));
+            services.AddSingleton(typeof(IMqttPublishService), typeof(MqttPublishService));
+
+            return services;
+        }
+    }
+}
