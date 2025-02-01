7a5e73cf2c05b41c103e0d8568105577a2d41574
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-02-01 15:44:19 +0200

    Add InvalidPressure constant to Constants.cs
    Introduced a new constant `InvalidPressure` of type `double` with a value of `-1` in the `Constants.cs` file. This constant is intended to represent an invalid pressure reading, complementing the existing `InvalidTemperature` and `InvalidHumidity` constants.

--- ESP32-NF-MQTT-DHT/Helpers/Constants.cs
+++ ESP32-NF-MQTT-DHT/Helpers/Constants.cs
@@ -7,6 +7,7 @@
         public const int SensorDataInterval = 300000;
         public const double InvalidTemperature = -50;
         public const double InvalidHumidity = -100;
+        public const double InvalidPressure = -1;
 
         public const int ReadInterval = 60000;
         public const int ErrorInterval = 30000;
