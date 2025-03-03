c5168563446b701a3b0b423308d9527b86838665
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-03-03 12:29:05 +0200

    Add DhtService class for DHT21 sensor management
    Introduces the `DhtService` class in the `ESP32_NF_MQTT_DHT.Services.Sensors` namespace.
    This class manages the DHT21 sensor, initializing it with GPIO pins 26 and 27.
    It overrides the method to return the sensor type as "DHT21" and implements data reading
    with error handling using `LogHelper` for logging errors and setting error values when necessary.

---
+++ ESP32-NF-MQTT-DHT/Services/Sensors/DhtService.cs
@@ -0,0 +1,54 @@
+﻿namespace ESP32_NF_MQTT_DHT.Services.Sensors
+{
+    using System;
+
+    using Helpers;
+
+    using Iot.Device.DHTxx.Esp32;
+
+    /// <summary>
+    /// Service for managing the DHT21 sensor.
+    /// </summary>
+    internal class DhtService : BaseSensorService
+    {
+        private const int PinEcho = 26;
+        private const int PinTrigger = 27;
+        private Dht21 _dht;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DhtService"/> class.
+        /// </summary>
+        public DhtService()
+        {
+            _dht = new Dht21(PinEcho, PinTrigger);
+        }
+
+        /// <summary>
+        /// Gets the type of the sensor.
+        /// </summary>
+        /// <returns>A string representing the sensor type.</returns>
+        public override string GetSensorType() => "DHT21";
+
+        /// <summary>
+        /// Reads the sensor data and updates the temperature and humidity values.
+        /// </summary>
+        protected override void ReadSensorData()
+        {
+            try
+            {
+                _temperature = _dht.Temperature.DegreesCelsius;
+                _humidity = _dht.Humidity.Percent;
+
+                if (!_dht.IsLastReadSuccessful)
+                {
+                    this.SetErrorValues();
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogError("Error in DhtServiceOptimized:", ex);
+                this.SetErrorValues();
+            }
+        }
+    }
+}
